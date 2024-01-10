a04d81d30239d68810089905791c3e69adc8cbd9
Anurag K. <student_ai_21_54@csmssengg.org>
2024-01-10 15:36:22 +0530

    Projects Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,15 @@ $(document).ready(function () {
     });
 
 });
+const cards = document.querySelectorAll('.cards_item');
+
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const cardTop = card.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+
+        if (cardTop < windowHeight) {
+            card.classList.add('slide-up');
+        }
+    });
+});
